76861238f41cebdbd04ded52b078d95f2b6149c8
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2025-01-07 11:17:34 -0500

    first commit

---
+++ src/main/java/com/_SDET_SeleniumPractice/App.java
@@ -0,0 +1,13 @@
+package com._SDET_SeleniumPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
